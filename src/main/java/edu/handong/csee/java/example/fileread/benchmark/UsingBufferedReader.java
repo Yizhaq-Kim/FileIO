47de7b38d7b845752914c51a1b56a1f27c326d60
BuildTools <unconfigured@null.spigotmc.org>
2021-05-20 10:51:33 +0900

    Lab10 commit

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -11,9 +11,13 @@ public class UsingBufferedReader {
 
         //-------------- Test reading 1 MB file. --------------------
 
+    	String fileName,fileName2;
+    	fileName=args[0]+".txt";
+    	fileName2=args[1]+".txt";
+    	
         StopWatch.start();
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+        BufferedReader inputStream= new BufferedReader(new FileReader(fileName));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
@@ -26,7 +30,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+        BufferedReader inputStream2= new BufferedReader(new FileReader(fileName2));
         while (inputStream2.read()!=-1){}
 
         long duration2 = StopWatch.stop();
